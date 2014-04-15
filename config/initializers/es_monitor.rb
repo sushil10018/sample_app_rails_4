69137f0403df2c16f3dcfa9c23c2567a760f6d37
Sushil Shrestha <sushil10018@gmail.com>
2014-04-15 18:38:07 +0545

    removed binding

--- config/initializers/es_monitor.rb
+++ config/initializers/es_monitor.rb
@@ -3,7 +3,6 @@ require "uri"
 require "net/http"
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
   if Rails.env.production? || Rails.env.development?
-    binding.pry
     USER_TOKEN = "0adc0060aa3dd9c"
     PROJECT_TOKEN = "7eeaa672e213267"
     API_URL = "http://localhost:3000/api/metrics"
