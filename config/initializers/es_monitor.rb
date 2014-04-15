3c087e1effec4efadb092d30214109ac64146957
Sushil Shrestha <sushil10018@gmail.com>
2014-04-15 17:48:01 +0545

    cleaned code used thread

--- config/initializers/es_monitor.rb
+++ config/initializers/es_monitor.rb
@@ -7,6 +7,9 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |na
     USER_TOKEN = "0adc0060aa3dd9c"
     PROJECT_TOKEN = "7eeaa672e213267"
     API_URL = "http://localhost:3000/api/metrics"
-    x = Net::HTTP.post_form(URI.parse("#{API_URL}?user_token=#{USER_TOKEN}&project_token=#{PROJECT_TOKEN}"), payload)
+    url = URI.parse("#{API_URL}?user_token=#{USER_TOKEN}&project_token=#{PROJECT_TOKEN}")
+    Thread.new do
+      x = Net::HTTP.post_form(url, payload)
+    end
   end
 end
